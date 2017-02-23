674f3eb6f49b94cbeec1619ae2f2c4c7fe5f535e
Jamie Adkins <jamieadkins95@gmail.com>
2017-02-23 14:38:28 +0000

    Fixed crash in Settings Activity
    Crash occured on rotation sue to improper state handling.
    
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

--- app/src/main/java/com/jamieadkins/gwent/settings/PreferenceFragment.java
+++ app/src/main/java/com/jamieadkins/gwent/settings/PreferenceFragment.java
@@ -8,6 +8,7 @@ import android.support.v7.preference.PreferenceFragmentCompat;
  */
 
 public class PreferenceFragment extends PreferenceFragmentCompat {
+    private static final String STATE_LAYOUT = "com.jamieadkins.gwent.layout";
     private int layout;
 
     public static PreferenceFragment newInstance(int layout) {
@@ -19,6 +20,17 @@ public class PreferenceFragment extends PreferenceFragmentCompat {
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         // Load the preferences from an XML resource.
-        addPreferencesFromResource(layout);
+        if (savedInstanceState == null) {
+            addPreferencesFromResource(layout);
+        } else {
+            layout = savedInstanceState.getInt(STATE_LAYOUT);
+            addPreferencesFromResource(layout);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        outState.putInt(STATE_LAYOUT, layout);
+        super.onSaveInstanceState(outState);
     }
 }
