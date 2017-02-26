9057b768da99f0c9fd284bb9db56583035eaaeca
Jamie Adkins <jamieadkins95@gmail.com>
2017-02-26 03:26:51 +0000

    Added equals method to Header class
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

--- commonutils/src/main/java/com/jamieadkins/commonutils/ui/Header.java
+++ commonutils/src/main/java/com/jamieadkins/commonutils/ui/Header.java
@@ -27,4 +27,9 @@ public class Header implements RecyclerViewItem {
     public int getItemType() {
         return TYPE_HEADER;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        return obj instanceof Header && ((Header) obj).mHeader.equals(mHeader);
+    }
 }
