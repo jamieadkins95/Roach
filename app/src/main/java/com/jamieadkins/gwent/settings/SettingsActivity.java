ce445998a91bed2f404064b12421a7d5b0ef942d
Jamie Adkins <jamieadkins95@gmail.com>
2017-03-20 16:42:58 +0000

    Added base for notifications
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

--- app/src/main/java/com/jamieadkins/gwent/settings/SettingsActivity.java
+++ app/src/main/java/com/jamieadkins/gwent/settings/SettingsActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.support.v7.preference.PreferenceManager;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 public class SettingsActivity extends BasePreferenceActivity implements
         SharedPreferences.OnSharedPreferenceChangeListener {
     public static final String PREFERENCE_ANALYTICS = "com.jamieadkins.gwent.analytics";
+    public static final String NOTIFICATIONS_NEWS = "com.jamieadkins.gwent.notifications.news";
+    public static final String NOTIFICATIONS_PATCH = "com.jamieadkins.gwent.notifications.patch";
 
     SharedPreferences mPreferences;
 
@@ -30,6 +33,22 @@ public class SettingsActivity extends BasePreferenceActivity implements
             case PREFERENCE_ANALYTICS:
                 boolean enableAnalytics = sharedPreferences.getBoolean(PREFERENCE_ANALYTICS, false);
                 FirebaseAnalytics.getInstance(this).setAnalyticsCollectionEnabled(enableAnalytics);
+                break;
+            case NOTIFICATIONS_NEWS:
+                boolean news = sharedPreferences.getBoolean(NOTIFICATIONS_NEWS, false);
+                if (news) {
+                    FirebaseMessaging.getInstance().subscribeToTopic("news");
+                } else {
+                    FirebaseMessaging.getInstance().unsubscribeFromTopic("news");
+                }
+                break;
+            case NOTIFICATIONS_PATCH:
+                boolean patch = sharedPreferences.getBoolean(NOTIFICATIONS_PATCH, false);
+                if (patch) {
+                    FirebaseMessaging.getInstance().subscribeToTopic("patch");
+                } else {
+                    FirebaseMessaging.getInstance().unsubscribeFromTopic("patch");
+                }
                 break;
         }
     }
