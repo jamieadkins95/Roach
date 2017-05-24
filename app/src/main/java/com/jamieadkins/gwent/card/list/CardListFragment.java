6d8cc90fab875e92ee2a0965d411d5c2ec9ae2a0
Jamie Adkins <jamieadkins95@gmail.com>
2017-05-24 23:04:17 +0100

    Temprary open beta message.
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

--- app/src/main/java/com/jamieadkins/gwent/card/list/CardListFragment.java
+++ app/src/main/java/com/jamieadkins/gwent/card/list/CardListFragment.java
@@ -2,8 +2,11 @@ package com.jamieadkins.gwent.card.list;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
+import android.view.View;
 
 import com.jamieadkins.gwent.R;
+import com.jamieadkins.gwent.base.SnackbarShower;
 
 /**
  * UI fragment that shows a list of the users decks.
@@ -14,5 +17,28 @@ public class CardListFragment extends BaseCardListFragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getActivity().setTitle(getString(R.string.card_database));
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (getActivity() != null) {
+            SnackbarShower snackbarShower = (SnackbarShower) getActivity();
+            if (snackbarShower != null) {
+                snackbarShower.showSnackbar(getString(R.string.open_beta),
+                        getString(R.string.info),
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                                builder.setMessage(R.string.open_beta_message)
+                                        .setTitle(R.string.open_beta)
+                                .setNegativeButton(getString(android.R.string.ok), null);
+                                AlertDialog dialog = builder.create();
+                                dialog.show();
+                            }
+                        });
+            }
+        }
     }
 }
