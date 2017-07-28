78bfe4c2fbbe89765043539a79bb1718dfbdfa96
Jamie Adkins <jamieadkins95@gmail.com>
2017-07-28 20:13:59 +0100

    Coverted BaseCardListFragment to Kotlin
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

---
+++ app/src/main/java/com/jamieadkins/gwent/card/list/BaseCardListFragment.kt
@@ -0,0 +1,71 @@
+package com.jamieadkins.gwent.card.list
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.jamieadkins.gwent.R
+import com.jamieadkins.gwent.base.BaseFragment
+import com.jamieadkins.gwent.card.CardFilter
+
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
+
+/**
+ * UI fragment that shows a list of the users decks.
+ */
+
+abstract class BaseCardListFragment : BaseFragment(), CardsContract.View {
+    private var mCardsPresenter: CardsContract.Presenter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val rootView = inflater!!.inflate(layoutId, container, false)
+        setupViews(rootView)
+        return rootView
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        super.onCreateOptionsMenu(menu, inflater)
+        setupFilterMenu(menu, inflater)
+    }
+
+    val layoutId: Int
+        get() = R.layout.fragment_card_list
+
+    override fun onStart() {
+        super.onStart()
+        onLoadData()
+    }
+
+    override fun onCardFilterUpdated() {
+        recyclerViewAdapter.clear()
+        onLoadData()
+    }
+
+    override fun onLoadData() {
+        super.onLoadData()
+        onLoadCardData()
+    }
+
+    fun onLoadCardData() {
+        val cardFilter = cardFilter
+        mCardsPresenter?.let {
+            it.getCards(cardFilter)
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(observer)
+        }
+    }
+
+    override fun setPresenter(presenter: CardsContract.Presenter) {
+        mCardsPresenter = presenter
+    }
+}
