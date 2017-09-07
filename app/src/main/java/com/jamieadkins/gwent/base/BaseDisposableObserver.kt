e982d354f74e7f195d7181e25fb67588e12ece71
Jamie Adkins <jamieadkins95@gmail.com>
2017-09-07 18:47:02 +0100

    Log to crashlytics when observable emits onError
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

--- app/src/main/java/com/jamieadkins/gwent/base/BaseDisposableObserver.kt
+++ app/src/main/java/com/jamieadkins/gwent/base/BaseDisposableObserver.kt
@@ -1,6 +1,7 @@
 package com.jamieadkins.gwent.base
 
 import android.util.Log
+import com.crashlytics.android.Crashlytics
 
 import io.reactivex.Observer
 import io.reactivex.disposables.Disposable
@@ -9,7 +10,7 @@ import io.reactivex.observers.DisposableObserver
 abstract class BaseDisposableObserver<T> : DisposableObserver<T>() {
 
     override fun onError(e: Throwable) {
-        Log.e(javaClass.simpleName, "onError", e)
+        Crashlytics.logException(e)
     }
 
     override fun onComplete() {
