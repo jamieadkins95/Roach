30d7bf67cd46e636fe2f00646b63a265786693f1
Jamie Adkins <jamieadkins95@gmail.com>
2018-09-09 18:52:17 +0100

    Added buildSrc
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,32 @@
+object Versions {
+
+    const val compile_sdk = 27
+    const val min_sdk = 16
+    const val target_sdk = 27
+    const val app_version_code = 48
+    const val app_version_name = "2.0.6"
+
+    const val gradle_plugin = "3.1.4"
+    const val google_services_plugin = "3.2.1"
+    const val kotlin = "1.2.61"
+    const val support_lib = "27.1.1"
+    const val support_constraint_layout = "1.1.0"
+
+    const val retrofit = "2.4.0"
+    const val rxjava = "2.1.12"
+    const val rxandroid = "2.0.2"
+    const val rxpreferences = "2.0.0"
+    const val gson = "2.8.2"
+    const val glide = "3.7.0"
+    const val nytimes_store = "3.0.1"
+    const val firebase = "15.0.2"
+    const val simple_stack = "1.9.3"
+    const val rx_activity = "0.4.5-2.x"
+    const val play_services = "15.0.1"
+    const val timber = "4.7.0"
+    const val junit = "4.12"
+    const val mockito = "2.18.3"
+    const val mockito_kotlin = "1.5.0"
+    const val support_test_runner = "1.0.1"
+    const val support_espresso = "3.0.1"
+}
