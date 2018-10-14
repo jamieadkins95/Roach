97756d239f05b70442f928a6e867209e8862a4f6
Jamie Adkins <jamieadkins95@gmail.com>
2018-10-15 00:46:39 +0100

    Removed patch table
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

--- database/src/main/java/com/jamieadkins/gwent/database/entity/PatchVersionEntity.kt
+++
@@ -1,8 +0,0 @@
-package com.jamieadkins.gwent.database.entity
-
-import android.arch.persistence.room.Entity
-import android.arch.persistence.room.PrimaryKey
-import com.jamieadkins.gwent.database.GwentDatabase
-
-@Entity(tableName = GwentDatabase.PATCH_VERSION_TABLE)
-data class PatchVersionEntity(@PrimaryKey val patch: String, val name: String, val lastUpdated: Long)
