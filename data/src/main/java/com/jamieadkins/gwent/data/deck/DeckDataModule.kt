183724e846b5f398338d479c792c75351d4a3d56
Jamie Adkins <jamieadkins95@gmail.com>
2018-10-23 20:23:53 +0100

    Singleton deck rep
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

--- data/src/main/java/com/jamieadkins/gwent/data/deck/DeckDataModule.kt
+++ data/src/main/java/com/jamieadkins/gwent/data/deck/DeckDataModule.kt
@@ -4,10 +4,12 @@ import com.jamieadkins.gwent.data.deck.repository.UserDeckRepository
 import com.jamieadkins.gwent.domain.deck.repository.DeckRepository
 import dagger.Binds
 import dagger.Module
+import javax.inject.Singleton
 
 @Module
 abstract class DeckDataModule {
 
     @Binds
+    @Singleton
     abstract fun repository(repository: UserDeckRepository): DeckRepository
 }
