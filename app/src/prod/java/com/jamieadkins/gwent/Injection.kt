b7af5099cdf6504e3cce4a6209c07a56c07b5190
Jamie Adkins <jamieadkins95@gmail.com>
2018-10-24 01:19:51 +0100

    Fixed prod implementation
    Signed-off-by: Jamie Adkins <jamieadkins95@gmail.com>

--- app/src/prod/java/com/jamieadkins/gwent/Injection.kt
+++ app/src/prod/java/com/jamieadkins/gwent/Injection.kt
@@ -6,32 +6,22 @@ import com.jamieadkins.commonutils.mvp2.BaseSchedulerProvider
 import com.jamieadkins.commonutils.mvp2.SchedulerProvider
 import com.jamieadkins.gwent.data.FactionMapper
 import com.jamieadkins.gwent.data.LocaleRepositoryImpl
-import com.jamieadkins.gwent.data.StoreManager
-import com.jamieadkins.gwent.data.card.mapper.ApiMapper
-import com.jamieadkins.gwent.data.card.mapper.ArtApiMapper
 import com.jamieadkins.gwent.data.card.mapper.GwentCardArtMapper
 import com.jamieadkins.gwent.data.card.mapper.GwentCardMapper
 import com.jamieadkins.gwent.data.card.repository.CardRepositoryImpl
-import com.jamieadkins.gwent.data.deck.mapper.DeckMapper
-import com.jamieadkins.gwent.data.deck.repository.UserDeckRepository
 import com.jamieadkins.gwent.data.filter.repository.FilterRepositoryImpl
-import com.jamieadkins.gwent.data.update.repository.CardUpdateRepository
-import com.jamieadkins.gwent.data.update.repository.CategoryUpdateRepository
-import com.jamieadkins.gwent.data.update.repository.KeywordUpdateRepository
-import com.jamieadkins.gwent.data.update.repository.PatchRepository
-import com.jamieadkins.gwent.data.update.repository.UpdateRepositoryImpl
 import com.jamieadkins.gwent.database.GwentDatabaseProvider
 import com.jamieadkins.gwent.domain.card.repository.CardRepository
-import com.jamieadkins.gwent.domain.deck.repository.DeckRepository
 import com.jamieadkins.gwent.domain.filter.repository.FilterRepository
-import com.jamieadkins.gwent.domain.update.repository.UpdateRepository
 import com.jamieadkins.gwent.main.GwentApplication
 
 object Injection {
 
     private val resources by lazy { GwentApplication.INSTANCE.applicationContext.resources }
     private val database by lazy { GwentDatabaseProvider.getDatabase(GwentApplication.INSTANCE.applicationContext) }
-    private val preferences by lazy { RxSharedPreferences.create(PreferenceManager.getDefaultSharedPreferences(GwentApplication.INSTANCE.applicationContext)) }
+    private val preferences by lazy {
+        RxSharedPreferences.create(PreferenceManager.getDefaultSharedPreferences(GwentApplication.INSTANCE.applicationContext))
+    }
     private val filterRepositories = mutableMapOf<String, FilterRepository>()
 
     private val factionMapper by lazy { FactionMapper() }
@@ -39,7 +29,6 @@ object Injection {
     private val cardMapper by lazy { GwentCardMapper(artMapper, factionMapper) }
 
     private val localeRepository by lazy { LocaleRepositoryImpl(preferences, resources) }
-
 
     fun provideCardRepository(): CardRepository {
         return CardRepositoryImpl(database, cardMapper, localeRepository)
