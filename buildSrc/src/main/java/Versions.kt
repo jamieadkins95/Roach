faa5ac99001e241cf15701176a2f7cdfd252f8bf
Jamie Adkins <jamieadkins95@gmail.com>
2019-07-27 17:06:49 +0100

    Bumped some versions

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -8,7 +8,7 @@ object Versions {
 
     const val gradle_plugin = "3.2.0"
     const val google_services_plugin = "4.1.0"
-    const val kotlin = "1.3.20"
+    const val kotlin = "1.3.41"
     const val support_lib = "28.0.0"
     const val support_constraint_layout = "1.1.2"
     const val dagger = "2.16"
