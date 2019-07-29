fbc53ace7ecbb7b55b42d5c403cd6d53537d4689
Jamie Adkins <jamieadkins95@gmail.com>
2019-07-29 17:33:44 +0100

    Show card name in gold if it's gold

--- decktracker/src/main/java/com/jamieadkins/gwent/decktracker/PredictedCardItem.kt
+++ decktracker/src/main/java/com/jamieadkins/gwent/decktracker/PredictedCardItem.kt
@@ -1,6 +1,8 @@
 package com.jamieadkins.gwent.decktracker
 
+import androidx.core.content.ContextCompat
 import com.jamieadkins.gwent.domain.card.model.GwentCard
+import com.jamieadkins.gwent.domain.card.model.GwentCardColour
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import kotlinx.android.synthetic.main.view_deck_analysis.*
@@ -15,6 +17,8 @@ data class PredictedCardItem(
 
     override fun bind(viewHolder: ViewHolder, position: Int) {
         viewHolder.name.text = card.name
+        val colour = if (card.colour == GwentCardColour.GOLD) R.color.gold else R.color.text_primary
+        viewHolder.name.setTextColor(ContextCompat.getColor(viewHolder.itemView.context, colour))
         viewHolder.tooltip.text = card.tooltip
         viewHolder.prediction.text = "$prediction%"
     }
--- decktracker/src/main/java/com/jamieadkins/gwent/decktracker/SimpleGwentCardItem.kt
+++ decktracker/src/main/java/com/jamieadkins/gwent/decktracker/SimpleGwentCardItem.kt
@@ -1,6 +1,8 @@
 package com.jamieadkins.gwent.decktracker
 
+import androidx.core.content.ContextCompat
 import com.jamieadkins.gwent.domain.card.model.GwentCard
+import com.jamieadkins.gwent.domain.card.model.GwentCardColour
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import kotlinx.android.synthetic.main.view_search_result.*
@@ -13,6 +15,8 @@ data class SimpleGwentCardItem(
 
     override fun bind(viewHolder: ViewHolder, position: Int) {
         viewHolder.name.text = card.name
+        val colour = if (card.colour == GwentCardColour.GOLD) R.color.gold else R.color.text_primary
+        viewHolder.name.setTextColor(ContextCompat.getColor(viewHolder.itemView.context, colour))
         viewHolder.tooltip.text = card.tooltip
         viewHolder.provisions.text = card.provisions.toString()
     }
