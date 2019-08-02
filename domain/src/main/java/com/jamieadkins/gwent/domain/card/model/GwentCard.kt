5a65f990994e76bd4fb98d350880957b95396fab
Jamie Adkins <jamieadkins95@gmail.com>
2019-08-02 14:57:35 +0100

    Style changes

--- domain/src/main/java/com/jamieadkins/gwent/domain/card/model/GwentCard.kt
+++ domain/src/main/java/com/jamieadkins/gwent/domain/card/model/GwentCard.kt
@@ -3,26 +3,25 @@ package com.jamieadkins.gwent.domain.card.model
 import com.jamieadkins.gwent.domain.GwentFaction
 
 data class GwentCard(
-        val id: String,
-        val name: String = "",
-        val tooltip: String = "",
-        val flavor: String = "",
-        val categories: List<String> = emptyList(),
-        val keywords: List<GwentKeyword> = emptyList(),
-        val faction: GwentFaction,
-        val strength: Int = 0,
-        val provisions: Int = 0,
-        val extraProvisions: Int = 0,
-        val colour: GwentCardColour,
-        val rarity: GwentCardRarity,
-        val type: GwentCardType,
-        val collectible: Boolean = false,
-        val craftCost: Int = 0,
-        val millValue: Int = 0,
-        val relatedCards: List<String> = emptyList(),
-        val cardArt: GwentCardArt) {
+    val id: String,
+    val name: String = "",
+    val tooltip: String = "",
+    val flavor: String = "",
+    val categories: List<String> = emptyList(),
+    val keywords: List<GwentKeyword> = emptyList(),
+    val faction: GwentFaction,
+    val strength: Int = 0,
+    val provisions: Int = 0,
+    val extraProvisions: Int = 0,
+    val colour: GwentCardColour,
+    val rarity: GwentCardRarity,
+    val type: GwentCardType,
+    val collectible: Boolean = false,
+    val craftCost: Int = 0,
+    val millValue: Int = 0,
+    val relatedCards: List<String> = emptyList(),
+    val cardArt: GwentCardArt
+) {
 
-    override fun toString(): String {
-        return name
-    }
+    override fun toString(): String = name
 }
