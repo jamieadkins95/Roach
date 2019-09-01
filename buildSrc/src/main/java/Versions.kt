f415f9c508a90c9524dc235b8b3dc37c95ab4b1c
Jamie Adkins <jamieadkins95@gmail.com>
2019-09-01 23:09:29 +0100

    Version bump

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -3,8 +3,8 @@ object Versions {
     const val compile_sdk = 28
     const val min_sdk = 16
     const val target_sdk = 28
-    const val app_version_code = 60
-    const val app_version_name = "3.3.2"
+    const val app_version_code = 61
+    const val app_version_name = "3.3.3"
 
     const val gradle_plugin = "3.5.0"
     const val google_services_plugin = "4.1.0"
